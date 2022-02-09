4eae69397b5a5712aa2ebee2eab62d12c0a22a91
Haatom <goldom1730@gmail.com>
2022-02-08 19:30:37 -0500

    Prueba de correción

--- src/main/resources/public/front.js
+++ src/main/resources/public/front.js
@@ -3,7 +3,7 @@ var front = (function(){
     return {
 
         connection : function(name, numero){
-            var url = 'https://conversortemperatura.herokuapp.com/' + name + '/' + numero
+            var url = 'https://conversortemperatura.herokuapp.com/'
                         fetch(url, {
                             method: 'GET',
                             headers: {
